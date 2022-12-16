2cf3d0f249b70f97969bc3b79edaba8aec39b318
Rizieq Fr <riziqfr111@gmail.com>
2022-12-16 10:33:31 +0700

    commit 1

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muh Rizieq Fazlulrahman Djafar" // TODO: replace this
+const studentId = "FE2611104" // TODO: replace this
+
+export { url, studentName, studentId }
