a5c9162aa144d3178b8c64a2117a7966e8436916
Rizieq Fr <riziqfr111@gmail.com>
2022-12-16 10:43:32 +0700

    edit url netlify

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://rizieq-assignment-deployment.netlify.app/" // TODO: replace this
 const studentName = "Muh Rizieq Fazlulrahman Djafar" // TODO: replace this
 const studentId = "FE2611104" // TODO: replace this
 
